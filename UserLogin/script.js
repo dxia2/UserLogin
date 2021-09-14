9b6918b17aacf0ed28ac4c4e29f948584e3e64aa
dxia2 <90279328+dxia2@users.noreply.github.com>
2021-09-14 12:21:51 -0600

    Add files via upload

--- UserLogin/script.js
+++ UserLogin/script.js
@@ -2,3 +2,25 @@
 
 //define varibles
 let loginButton = document.getElementById("loginButton");
+let usernameInput = document.getElementById("usernameInput");
+let passwordInput = document.getElementById("passwordInput");
+
+let correctUsername = "admin";
+let correctPassword = "1234";
+
+//event listener
+loginButton.addEventListener("click", attemptLogin);
+
+function attemptLogin() {
+    // check if username and password is correct
+    if (usernameInput.value === correctUsername && passwordInput.value === correctPassword) {
+        //username and password is correct
+        alert("Login Successful");
+    } else if (usernameInput.value != correctUsername) {
+        //username is incorrect, password doesn't matter
+        alert("Invalid Username");
+    } else {
+        //username correct and password incorrect
+        alert("Invalid Password");
+    }
+}
